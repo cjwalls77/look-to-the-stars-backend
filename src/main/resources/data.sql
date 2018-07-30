ff9154abcbda392fb2cf2bdf8508ea14564e277c
C.J. Walls <cjwalls77@gmail.com>
2018-07-30 01:55:37 -0400

    Update Planet Data
    Update planet data in h2 database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,26 @@
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (1, 'Mercury', 4, 100, 'rgb(189, 137, 62)', 'First planet.');
+  VALUES (1, 'Mercury', 4, 100, 'rgb(189, 137, 62)', 'First planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (2, 'Venus', 5, 120, 'rgb(243, 167, 130)', 'Second planet.');
+  VALUES (2, 'Venus', 5, 120, 'rgb(243, 167, 130)', 'Second planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (3, 'Earth', 6, 140, 'rgb(123, 193, 94)', 'Third planet.');
+  VALUES (3, 'Earth', 6, 140, 'rgb(123, 193, 94)', 'Third planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (4, 'Mars', 4, 160, 'rgb(189, 70, 63)', 'Fourth planet.');
+  VALUES (4, 'Mars', 4, 160, 'rgb(189, 70, 63)', 'Fourth planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (5, 'Jupiter', 15, 180, 'rgb(225, 141, 131)', 'Fifth planet.');
+  VALUES (5, 'Jupiter', 15, 180, 'rgb(225, 141, 131)', 'Fifth planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (6, 'Saturn', 10, 200, 'rgb(165, 139, 182)', 'Sixth planet.');
+  VALUES (6, 'Saturn', 10, 200, 'rgb(165, 139, 182)', 'Sixth planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (7, 'Uranus', 9, 220, 'rgb(173, 190, 179)', 'Seventh planet.');
+  VALUES (7, 'Uranus', 9, 220, 'rgb(173, 190, 179)', 'Seventh planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
-  VALUES (8, 'Neptune', 7, 240, 'rgb(97, 152, 210)', 'Eight planet.');
+  VALUES (8, 'Neptune', 7, 240, 'rgb(97, 152, 210)', 'Eighth planet from the sun.');
 
 INSERT INTO PLANET (ID, NAME, RADIUS, ORBIT_RADIUS, COLOR, DESCRIPTION)
   VALUES (9, 'Pluto', 3, 260, 'rgb(250, 228, 155)', '??? planet... Ok not a planet but including it anyway :)');
